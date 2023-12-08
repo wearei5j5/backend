dbb0fcccdd1db904f6f00e69442436a86868db5d
Duhan Mo <moduhan95@gmail.com>
2023-12-08 17:40:09 +0900

    fix: add activeProfiles into Test

--- src/test/kotlin/org/dev/i5j5/I5j5ApplicationTests.kt
+++ src/test/kotlin/org/dev/i5j5/I5j5ApplicationTests.kt
@@ -2,8 +2,10 @@ package org.dev.i5j5
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class I5j5ApplicationTests {
 
     @Test
