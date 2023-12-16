44a729cf425b19b474bbd5164e1353dc57cac0ad
Duhan Mo <devduhanmo@gmail.com>
2023-12-17 00:19:19 +0900

    Add flyway in product environment #25

--- src/main/kotlin/org/dev/otte/review/command/domain/Review.kt
+++ src/main/kotlin/org/dev/otte/review/command/domain/Review.kt
@@ -2,12 +2,15 @@ package org.dev.otte.review.command.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType.STRING
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Table
 import org.dev.otte.common.domain.BaseEntity
 
 @Entity
 @Table(name = "review")
 class Review(
-    @Column
+    @Column(columnDefinition = "varchar(50)")
+    @Enumerated(STRING)
     val satisfaction: ReviewSatisfaction
 ) : BaseEntity()
