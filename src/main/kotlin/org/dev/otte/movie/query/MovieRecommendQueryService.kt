d5e9d3d78d9b79cddefa2a7d43f65f7bcc9bc73d
Duhan Mo <moduhan95@gmail.com>
2023-12-18 10:14:05 +0900

    Fix clova request time to 700ms

--- src/main/kotlin/org/dev/otte/movie/query/MovieRecommendQueryService.kt
+++ src/main/kotlin/org/dev/otte/movie/query/MovieRecommendQueryService.kt
@@ -46,7 +46,7 @@ class MovieRecommendQueryService(
         }
 
         while (removeWhitespaceMovieNameSet.size < 3) {
-            Thread.sleep(1000)
+            Thread.sleep(700)
             val movieRecommendQueryResponse = movieRecommendQueryResponse(movieRecommendRequest)
 
 
