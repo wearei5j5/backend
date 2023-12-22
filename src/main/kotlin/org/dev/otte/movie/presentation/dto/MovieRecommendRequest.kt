b7697c72a1823fcf45f014e14a4e471d7d66237b
Duhan Mo <moduhan95@gmail.com>
2023-12-22 10:58:06 +0900

    Fix limit of movie recommend input #38

--- src/main/kotlin/org/dev/otte/movie/presentation/dto/MovieRecommendRequest.kt
+++ src/main/kotlin/org/dev/otte/movie/presentation/dto/MovieRecommendRequest.kt
@@ -11,10 +11,10 @@ data class MovieRecommendRequest(
     @field:NotEmpty(message = "Input ott list can not be empty")
     var ottList: List<Ott>,
     @Schema(description = "감정", example = "지금 기분이 우울해")
-    @field:Size(min = 1, max = 30)
+    @field:Size(min = 1, max = 100)
     val feeling: String,
     @Schema(description = "상황", example = "기분좋게 해줄 영화를 찾고 있어")
-    @field:Size(min = 1, max = 30)
+    @field:Size(min = 1, max = 100)
     val situation: String
 ) {
     fun toCondition(userId: Long?): MovieRecommendCondition {
