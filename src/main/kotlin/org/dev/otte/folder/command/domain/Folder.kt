6b836dc8de33fc7b4c61a66c1cbb920776bf6870
Duhan Mo <devduhanmo@gmail.com>
2024-01-07 16:33:25 +0900

    Add folder domain #40

---
+++ src/main/kotlin/org/dev/otte/folder/command/domain/Folder.kt
@@ -0,0 +1,16 @@
+package org.dev.otte.folder.command.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import org.dev.otte.common.domain.BaseEntity
+
+@Entity
+@Table(name = "folder")
+class Folder(
+    @Column(name = "userId")
+    val userId: Long,
+
+    @Column(name = "name")
+    val name: String
+) : BaseEntity()
