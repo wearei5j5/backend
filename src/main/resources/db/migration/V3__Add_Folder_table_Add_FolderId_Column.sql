75e444e67e4276cfd7e9e21efd9fc546bdbda10d
Duhan Mo <devduhanmo@gmail.com>
2024-01-07 17:20:08 +0900

    Add flyway #40

---
+++ src/main/resources/db/migration/V3__Add_Folder_table_Add_FolderId_Column.sql
@@ -0,0 +1,12 @@
+create table folder
+(
+    id         bigint not null auto_increment,
+    user_id    bigint,
+    name       varchar(255),
+    created_at timestamp(6),
+    updated_at timestamp(6),
+    primary key (id)
+);
+
+alter table movie
+    add folderId bigint null;
